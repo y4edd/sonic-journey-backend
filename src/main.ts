4da4ac64969275db6d9612fc85ac8e05b9339802
y4edd <yaeyuu0116@icloud.com>
2025-04-01 16:46:31 +0900

    feat: main.tsの設定

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
+  // AppModule を使って NestJS アプリケーションのインスタンス（app）を作成する
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // ValidationPipe というNestJSに組み込まれているパイプクラスを使って、
+  // リクエストの入力値をアプリ全体でチェックする
+  // 「whitelist: true」によってDTOで定義されていないプロパティは自動的に削除される
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  // CORSの設定
+  app.enableCors({
+    // クッキーや認証情報（セッション情報、トークンなど）を
+    // フロントエンドからサーバーに送ることを許可する
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  });
+
+  // フロントエンドからうけとったcookieを解析できるように
+  // ミドルウェアでcookieParserを実行しておく
+  app.use(cookieParser);
+
+  await app.listen(process.env.PORT ?? 3005);
 }
 bootstrap();
