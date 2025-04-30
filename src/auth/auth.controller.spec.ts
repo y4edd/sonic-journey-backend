97e2760ca836e8c716ab8681d4e55f34259f509d
y4edd <yaeyuu0116@icloud.com>
2025-04-30 17:40:49 +0900

    feat: authのコントローラテスト

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { Response } from 'express';
+import { mock } from 'jest-mock-extended';
+import { AuthDTO } from './interfaces/auth.interface';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
+  const mockRes = mock<Response>();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            login: jest.fn().mockResolvedValue({ accessToken: 'test-token' }),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get(AuthController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('POST /auth/login', () => {
+    test('cookie が設定される', async () => {
+      await authController.login(
+        { email: 'test@test.com', password: 'testtest' } as AuthDTO,
+        mockRes,
+      );
+
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      expect(mockRes.cookie).toHaveBeenCalled(); // 呼ばれた回数が気になるなら
+    });
+  });
+
+  describe('POST /auth/logout', () => {
+    test('cookie が削除される', () => {
+      authController.logout(mockRes);
+
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      expect(mockRes.clearCookie).toHaveBeenCalled();
+    });
   });
 });
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -21,7 +21,6 @@ export class AuthController {
     @Body() dto: AuthDTO,
     @Res({ passthrough: true }) res: Response,
   ) {
-    console.log('login処理始まる');
     const jwt = await this.authService.login(dto);
     res.cookie('access_token', jwt.accessToken, {
       httpOnly: true,
@@ -38,7 +37,6 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   @Post('logout')
   logout(@Res({ passthrough: true }) res: Response) {
-    console.log('logout処理始まる');
     res.clearCookie('access_token', {
       httpOnly: true,
       secure: false,
